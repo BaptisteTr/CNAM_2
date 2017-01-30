e70a8b9cc88e8515adc3e77a7bd9efc2ed42f673
BaptisteTr <baptiste.trautmann@etu.unistra.fr>
2017-01-30 23:57:31 +0100

    doc doxygen générée

--- PROJETS/SYS/PROJET/src/implementation/util.c
+++ PROJETS/SYS/PROJET/src/implementation/util.c
@@ -58,6 +58,15 @@ char* dup_optarg_str()
   return str;
 }
 
+/**
+ * \fn adx_store_data(const char * filepath, const char * data)
+ *
+ * \brief output the string data in the file at path "filepath"
+ * \param filepath path of the file to output in
+ * \param data string to write in the file
+ *
+ * \return void
+ */
 void adx_store_data(const char *filepath, const char *data)
 {
   FILE *fp = fopen(filepath, "ab");
@@ -68,6 +77,17 @@ void adx_store_data(const char *filepath, const char *data)
   }
 }
 
+
+/**
+ * \fn int split_line (const char * input_line, char * split_char, char *** res)
+ *
+ * \brief split the input for each char "split_char" found and set each resulting string in res
+ * \param input_line input to process
+ * \param split_char char to check for splitting
+ * \param res adress of the string array to set the results in
+ *
+ * \return number of splits
+ */
 int split_line(const char* input_line, char* split_char, char*** res){
 
   char ** return_tab;
@@ -98,6 +118,15 @@ int split_line(const char* input_line, char* split_char, char*** res){
   return n_spaces;
 }
 
+
+/**
+ * \fn int is_built_in (const char * command)
+ *
+ * \brief test if the command is built_in
+ * \param command string to check
+ *
+ * \return 0 == TRUE, 1 == FALSE
+ */
 int is_built_in(const char* command){
   /*if(strcmp("cd",command) != 0){
 
@@ -119,15 +148,34 @@ int is_built_in(const char* command){
   return 1;
 }
 
+/**
+ * \fn char * concat (const char * s1, const char * s2)
+ *
+ * \brief concatenate two string and return the result
+ * \param s1 first string to prepend
+ * \param s2 second string to append
+ *
+ * \return a new string resulting of the concatenation of the two
+ */
 char* concat(const char *s1, const char *s2)
 {
-    char *result = malloc(strlen(s1)+strlen(s2)+1);//+1 for the zero-terminator
-    //in real code you would check for errors in malloc here
+    char *result = malloc(strlen(s1)+strlen(s2)+1);
+
     strcpy(result, s1);
     strcat(result, s2);
     return result;
 }
 
+/**
+ * \fn void substring_from_pos (const char * input_line, int pos_start, int pos_end)
+ *
+ * \brief substract string from pos_start to pos_end in the input
+ * \param input_line input to process
+ * \param pos_start position to start substracting
+ * \param pos_end position to stop substracting
+ *
+ * \return the string substracted
+ */
 char * substring_from_pos(char * input_line, int pos_start, int pos_end){
 
   char * res = malloc(sizeof(char) * ((pos_end-pos_start)+1));
@@ -150,6 +198,17 @@ char * substring_from_pos(char * input_line, int pos_start, int pos_end){
 
 }
 
+/**
+ * \fn void substring_from_pointer_to_pointer (const char * input_line, const char * pointer_start, const char * pointer_end, char ** res)
+ *
+ * \brief copy the value of the substring contained in the input from two pointers and set it in res
+ * \param input_line input to process
+ * \param pointer_start pointer to start substracting
+ * \param pointer_end pointer to stop substracting
+ * \param res adress to set the result in
+ *
+ * \return void
+ */
 void substring_from_pointer_to_pointer(const char * input_line,const char * pointer_start,const char * pointer_end, char ** res){
 
   char * substring = malloc(sizeof(char) * (pointer_end-pointer_start+1));
@@ -175,6 +234,15 @@ void substring_from_pointer_to_pointer(const char * input_line,const char * poin
 
 }
 
+/**
+ * \fn int is_substring (const char * substring, const char * main_string)
+ *
+ * \brief check if the substring is found at the beginning of main_string
+ * \param substring substring to search for
+ * \param main_string string to process
+ *
+ * \return 0 == TRUE, 1 == FALSE
+ */
 int is_substring(const char * substring, const char * main_string){
 
   int i = 0;
@@ -196,6 +264,15 @@ int is_substring(const char * substring, const char * main_string){
     
 }
 
+/**
+ * \fn char * get_substring_adress_in_string (const char * substring, const char * input_string)
+ *
+ * \brief return the adress of the first character of the substring found in input_string
+ * \param substring substring to search for
+ * \param input_string string to process
+ *
+ * \return the adress of the substring's first char in the substring found
+ */
 char * get_substring_adress_in_string(const char * substring, const char * input_string){ // Récupérer l'adresse de la première occurence de substring dans input_string
 
   char * res = NULL;
@@ -214,6 +291,15 @@ char * get_substring_adress_in_string(const char * substring, const char * input
 
 }
 
+/**
+ * \fn void remove_substring_post_string (const char* c, char ** string_to_sub)
+ *
+ * \brief modify a string to remove all characters after the string c is found
+ * \param c string to search for
+ * \param string_to_sub adress of the string to modify
+ *
+ * \return void
+ */
 void remove_substring_post_string(const char* c, char ** string_to_sub){
 
   char * res = *string_to_sub;
@@ -231,6 +317,15 @@ void remove_substring_post_string(const char* c, char ** string_to_sub){
 
 }
 
+/**
+ * \fn void remove_substring_post_char (const char c, char ** string_to_sub)
+ *
+ * \brief modify a string to remove all characters after the character c is found
+ * \param c character to search for
+ * \param string_to_sub adress of the string to modify
+ *
+ * \return void
+ */
 void remove_substring_post_char(const char c, char ** string_to_sub){
 
   char * res = *string_to_sub;
@@ -254,6 +349,16 @@ void remove_substring_post_char(const char c, char ** string_to_sub){
 
 }
 
+/**
+ * \fn int split_by_char (char * input_line, char c, char *** substring_tab)
+ *
+ * \brief split the input for each time the character c is found and set the result in substring_tab
+ * \param input_line the string to process
+ * \param character the character to search for
+ * \param substring_tab the adress of the string array to put the results in
+ *
+ * \return the number of splits
+ */
 int split_by_char(char * input_line, char c, char *** substring_tab){
 
   char ** res = NULL;
