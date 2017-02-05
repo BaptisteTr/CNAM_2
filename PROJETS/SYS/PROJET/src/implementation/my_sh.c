aceabdd3b723bed30ad44ea9303f4b95115cde5a
BaptisteTr <baptiste.trautmann@etu.unistra.fr>
2017-02-05 23:26:01 +0100

    Réparation, ajout perror

--- PROJETS/SYS/PROJET/src/implementation/my_sh.c
+++ PROJETS/SYS/PROJET/src/implementation/my_sh.c
@@ -16,6 +16,7 @@
 #include<fcntl.h>
 #include "util.h"
 #include "my_sh.h"
+#include "cd.c"
 
 #include<time.h>
 #include<getopt.h>
@@ -50,6 +51,76 @@
 void print_usage(char* bin_name)
 {
   dprintf(1, "USAGE: %s %s\n\n%s\n", bin_name, USAGE_SYNTAX, USAGE_PARAMS);
+}
+
+/**
+ * \fn int fork_pipes (int n, char ** input_line)
+ *
+ * \brief execute the list of input sequentially and pipe each output in the next input
+ * \param n number of iteration
+ * \param **input_line array of string to work on
+ *
+ * \return execution result : 0 = OK
+ */
+int fork_pipes (int n, char ** input)
+{
+  int i, res;
+  pid_t pid;
+  int in, fd [2];
+
+  /* The first process should get its input from the original file descriptor 0.  */
+  in = 0;
+
+  /* Note the loop bound, we spawn here all, but the last stage of the pipeline.  */
+  for (i = 0; i < n - 1; ++i)
+    {
+      pipe (fd);
+
+      /* f [1] is the write end of the pipe, we carry `in` from the prev iteration.  */
+      init_pipe (in, fd [1], input[i]);
+
+      /* No need for the write end of the pipe, the child will write here.  */
+      close (fd [1]);
+
+      /* Keep the read end of the pipe, the next child will read from there.  */
+      in = fd [0];
+    }
+
+  /* Last stage of the pipeline - set stdin be the read end of the previous pipe
+     and output to the original file descriptor 1. */  
+  if (in != 0)
+    dup2 (in, 0);
+
+  /* Execute the last stage with the current process. */
+
+  res = main_exec(input [i]);
+
+  close(in);
+  close(fd[1]);
+  close(fd[2]);
+  dup2(0,0);
+  dup2(1,1);
+
+  return res;
+}
+
+/**
+ * \fn int is_built_in (const char * command)
+ *
+ * \brief test if the command is built_in
+ * \param command string to check
+ *
+ * \return 0 == TRUE, 1 == FALSE
+ */
+int exec_if_built_in(const char* command){
+
+  if(strncmp("pwd", command,3) == 0){
+    return pwd();
+  } else if(strncmp("echo", command,4) == 0){
+    return echo(command+5);
+  } else {
+    return 1;
+  }
 }
 
 /**
@@ -88,15 +159,16 @@ int exec_semicolon(char * input_line){
 int exec_background(char * input_line){
 
   remove_substring_post_char('&', &input_line);
+  int res;
   
   int pid_fork = fork();
 
   if(pid_fork == 0){ // Fils
 
-    main_exec(input_line); // ps
+    res = main_exec(input_line); // ps
     exit(0);
   }
-  return EXIT_SUCCESS;
+  return res;
 }
 
 /**
@@ -109,48 +181,48 @@ int exec_background(char * input_line){
  */
 int exec_logical_operator(char * input_line){
 
-  char * pointer_and;
-  char * pointer_or;
-  char ** res;
+  char * pointer_and, pointer_or;
+  char ** res_split;
+  int res;
 
   pointer_and = get_substring_adress_in_string("&&", input_line); // Renommer en is_string_present
   pointer_or = get_substring_adress_in_string("||", input_line);
 
   if(pointer_and == NULL && pointer_or != NULL){
 
-    split_by_string_first_occurence(input_line,"||", &res);
+    split_by_string_first_occurence(input_line,"||", &res_split);
 
-    if(main_exec(res[0]) == 1){
-      main_exec(res[1]);
+    if(main_exec(res_split[0]) == 1){
+      res = main_exec(res_split[1]);
     }
 
   } else if(pointer_and != NULL && pointer_or == NULL){
 
-    split_by_string_first_occurence(input_line,"&&", &res);
+    split_by_string_first_occurence(input_line,"&&", &res_split);
 
-    if(main_exec(res[0]) == 0){
-      main_exec(res[1]);
+    if(main_exec(res_split[0]) == 0){
+      res = main_exec(res_split[1]);
     }
 
   } else if(pointer_and < pointer_or){
 
-    split_by_string_first_occurence(input_line,"&&", &res);
+    split_by_string_first_occurence(input_line,"&&", &res_split);
 
-    if(main_exec(res[0]) == 0){
-      main_exec(res[1]);
+    if(main_exec(res_split[0]) == 0){
+      res = main_exec(res_split[1]);
     }
 
   } else if(pointer_and > pointer_or){
 
-    split_by_string_first_occurence(input_line,"||", &res);
+    split_by_string_first_occurence(input_line,"||", &res_split);
 
-    if(main_exec(res[0]) == 1){
-      main_exec(res[1]);
+    if(main_exec(res_split[0]) == 1){
+      res = main_exec(res_split[1]);
     }
 
   }
 
-  return EXIT_SUCCESS;
+  return res;
 }
 
 /**
@@ -161,11 +233,11 @@ int exec_logical_operator(char * input_line){
  *
  * \return execution result : 0 = OK
  */
-void exec_redirection(char * input_line){
+int exec_redirection(char * input_line){
 
   char * pointer_left_can, pointer_right_can, pointer_double_left_can, pointer_double_right_can;
-  char ** res;
-  int out, is_double = 0;
+  char ** res_split;
+  int res, out, is_double = 0;
 
   pointer_left_can = get_substring_adress_in_string("<", input_line); // Renommer en is_string_present
   pointer_right_can = get_substring_adress_in_string(">", input_line);
@@ -175,72 +247,67 @@ void exec_redirection(char * input_line){
   {
 
     if(*(pointer_left_can+2) == '<'){
-      split_by_string_first_occurence(input_line,"<<", &res);      
+      split_by_string_first_occurence(input_line,"<<", &res_split);      
       is_double = 1;
     } else {
-      split_by_char_first_occurence(input_line,'<', &res);
+      split_by_char_first_occurence(input_line,'<', &res_split);
     }
 
-    if( is_valid_path(res[0]) == 0 ) { // Améliorer (tester si il s'agit d'une commande ou d'un fichier)
+    if( is_valid_path(res_split[0]) == 0 ) { 
 
-      clean_spaces(res[0]);
+      clean_spaces(res_split[0]);
 
       if(is_double == 1) {
-        out = open(res[0], O_RDWR|O_CREAT|O_APPEND, 06755);
+        out = open(res_split[0], O_RDWR|O_CREAT|O_APPEND, 06755);
       } else {
-        out = open(res[0], O_RDWR|O_CREAT|O_TRUNC, 06755);
+        out = open(res_split[0], O_RDWR|O_CREAT|O_TRUNC, 06755);
       }
       if (-1 == out) { perror("Impossible de créer le fichier ou de l'ouvrir"); return 255;}
 
       int save_out = dup(fileno(stdout));
 
-      if (-1 == dup2(out, fileno(stdout))) { perror("cannot redirect stdout"); return 255; }
+      if (-1 == dup2(out, fileno(stdout))) { perror("Impossible de rediriger stdout"); return 255; }
 
-
-      main_exec(res[1]);
+      res = main_exec(res_split[1]);
 
       fflush(stdout); close(out);
       dup2(save_out, fileno(stdout));
       close(save_out);
 
 
-    } else if (res[0][0] == '2') { // Tester entier
+    } else if (res_split[0][0] == '2') {
 
 
       dup2(2, 1);
 
-      main_exec(res[1]);
+      res = main_exec(res_split[1]);
 
       dup2(1, 1);
-    } else { // Rediriger dans un pipe (fork un fils qu'on attend et qui prend le pere en input? )
+    } else { 
 
-      fork_pipes(2,res);
+      res = fork_pipes(2,res_split);
 
     }
-
-
-    //Tester la partie de gauche (fichier, output ou commande)
-
 
   } else {
 
     if(*(get_substring_adress_in_string(">", input_line)+2) == '>'){
-      split_by_string_first_occurence(input_line,">>", &res);      
+      split_by_string_first_occurence(input_line,">>", &res_split);      
       is_double = 1;
     } else {
-      split_by_char(input_line,'>', &res);
+      split_by_char(input_line,'>', &res_split);
     }
 
-    if( is_valid_path(res[1]) == 0) {
+    if( is_valid_path(res_split[1]) == 0) {
 
 
-      clean_spaces(&res[1]);
+      clean_spaces(&res_split[1]);
 
 
       if(is_double == 1) {
-        out = open(res[1], O_RDWR|O_CREAT|O_APPEND, 06755);
+        out = open(res_split[1], O_RDWR|O_CREAT|O_APPEND, 06755);
       } else {
-        out = open(res[1], O_RDWR|O_CREAT|O_TRUNC, 06755);
+        out = open(res_split[1], O_RDWR|O_CREAT|O_TRUNC, 06755);
       }
       
       if (-1 == out) { perror("Impossible de créer le fichier ou de l'ouvrir"); return 255;}
@@ -249,7 +316,7 @@ void exec_redirection(char * input_line){
       if (-1 == dup2(out, fileno(stdout))) { perror("cannot redirect stdout"); return 255; }
 
 
-      main_exec(res[0]);
+      res = main_exec(res_split[0]);
 
       fflush(stdout); close(out);
       dup2(save_out, fileno(stdout));
@@ -257,20 +324,20 @@ void exec_redirection(char * input_line){
 
 
         // file exists
-    } else if (res[1][0] == '2') {
+    } else if (res_split[1][0] == '2') {
       dup2(2, 1);
 
-      main_exec(res[0]);
+      res = main_exec(res_split[0]);
 
       dup2(1, 1);
     } else {
 
-      fork_pipes(2,res);
+      fork_pipes(2,res_split);
     }
 
   }
 
-  return EXIT_SUCCESS;
+  return res;
 }
 
 /**
@@ -281,17 +348,17 @@ void exec_redirection(char * input_line){
  *
  * \return execution result : 0 = OK
  */
-void exec_piping(char * input_line){
+int exec_piping(char * input_line){
 
 
   char ** result_pipe_split;
-  int nb_split;
+  int nb_split, res;
 
   nb_split = split_by_char(input_line, '|', &result_pipe_split); // ( ps | grep sh | grep ??) => [0]["ps"] [1]["grep sh"] [2]["grep ??"]
 
-  fork_pipes(nb_split+1, result_pipe_split);
+  res = fork_pipes(nb_split+1, result_pipe_split);
 
-  return EXIT_SUCCESS;
+  return res;
 }
 
 
@@ -304,35 +371,49 @@ void exec_piping(char * input_line){
  * \return execution result : 0 = OK
  */
 int exec_command(const char* parameters){
-  /*if(is_built_in(commande) == 0){
-    printf("TODO");
-  } else{*/
   char* command_path = "/bin/";
   char* command_final;
+  char* full_string = malloc(BUFFER_SIZE);
   char ** parameters_final;
   int i = 0, nb_arg, res;
+
+  strcpy(full_string, parameters);
 
   nb_arg = split_line(parameters, " ", &parameters_final); 
 
   command_final = parameters_final[0];
 
   parameters_final[0] = command_final;
-  /*while(i<nb_arg){
 
-    printf("parameter : %s\n",parameters_final[i+1]);
+  if(strncmp("cd ",full_string,3) == 0){
 
-    parameters_final[i] = parameters_final[i+1];
-    i++;
-  }*/
+    clean_spaces(&full_string);
+    struct stat sb;
+    
+    if (stat(full_string+3, &sb) == 0 && S_ISDIR(sb.st_mode))
+    {  
+      return chdir(full_string+3);
+    }
+    else
+    {
+      errno = ENOTDIR;
+      perror("");
+      return 1;
+    }
 
-  //parameters_final[nb_arg] = NULL;
-
+  } else if(strncmp("exit",full_string,4) == 0){
+    exit(0);
+  }
 
   pid_t pid = fork();
 
   if (pid == 0) {
-      
-    res = execvp (parameters_final[0], parameters_final);
+
+    if(res = exec_if_built_in(full_string) == 0){
+
+    } else{
+      res = execvp (parameters_final[0], parameters_final);
+    }
 
     if(res == -1){
       printf("%s: commande introuvable\n", command_final);
@@ -340,11 +421,11 @@ int exec_command(const char* parameters){
   }
   else{ 
     int status = -1;
-
-    wait(&status); 
+    wait(&status);
+    res = status; 
   }
 
-    return res;
+  return res;
 }
 
 
@@ -359,32 +440,33 @@ int exec_command(const char* parameters){
 int main_exec(char * input_line)
 {
 
+  int res;
+
   if( strstr(input_line,";")){
-    exec_semicolon(input_line); // Lance autant d'exec de que bloc
+    res = exec_semicolon(input_line); // Lance autant d'exec de que bloc
   } 
   
   else if( input_line[strlen(input_line)-1] == '&'){
-    exec_background(input_line);// Lance l'exec dans un processus fils sans attente
+    res = exec_background(input_line);// Lance l'exec dans un processus fils sans attente
   }
   
   else if( strstr(input_line, "&&") || strstr(input_line, "||")){
-    exec_logical_operator(input_line); // Lance des exec pour chaque bloc logique et test leurs validités
+    res = exec_logical_operator(input_line); // Lance des exec pour chaque bloc logique et test leurs validités
   }
   
   else if( strstr(input_line," >") || strstr(input_line," >>") || strstr(input_line, "< ") || strstr(input_line, "<< ")){
-    exec_redirection(input_line); // Lance une chaine d'exec avec des redirections d'output/input
+    res = exec_redirection(input_line); // Lance une chaine d'exec avec des redirections d'output/input
   }
   
   else if( strstr(input_line, " | ")){
-    exec_piping(input_line);  // Lance des execs chainés dans des pipes
+    res = exec_piping(input_line);  // Lance des execs chainés dans des pipes
   }
   
   else{
-
-    exec_command(input_line); // Lance une éxécution simple de la commande passée (built-in ou bash)
+    res = exec_command(input_line); // Lance une éxécution simple de la commande passée (built-in ou bash)
   }
   
-  return EXIT_SUCCESS;
+  return res;
 
 }
 
--- PROJETS/SYS/PROJET/src/implementation/util.c
+++ PROJETS/SYS/PROJET/src/implementation/util.c
@@ -35,6 +35,65 @@ void free_if_needed(void* to_free)
   if (to_free != NULL) free(to_free);  
 }
 
+/**
+ * print on standard output the passed parameter
+ *
+ * \param char * passed string
+ * \return int 0 == SUCCESS
+ */
+int echo(char * input){
+
+  printf("%s\n",input);
+
+  return EXIT_SUCCESS;
+}
+
+/**
+ * print the current directory on standard output
+ *
+ * \return int 0 == SUCCESS
+ */
+int pwd(){
+
+  char * currentDir = malloc(BUFFER_SIZE);
+  currentDir = getcwd(currentDir,BUFFER_SIZE);
+  dprintf(STDOUT,"%s\n",currentDir);
+  free(currentDir);
+  return EXIT_SUCCESS;
+}
+
+/**
+ * Change the current Directory
+ *
+ * \param char * directory target directory
+ * \return int 0 == SUCCESS
+ */
+int changeDir(const char * directory){
+
+  char * currentDir = malloc(BUFFER_SIZE);
+  char * changeD = malloc(BUFFER_SIZE);
+  char * res = malloc(BUFFER_SIZE);
+
+  strcpy(changeD,directory);
+
+  if(directory[0] == '/'){ // Corriger
+    strcpy(res, changeD);
+  } else {
+    currentDir = getcwd(currentDir,BUFFER_SIZE);
+    strcpy(res,currentDir);
+    strcat(res,"/");
+    strcat(res,changeD);
+  }
+
+  chdir(res);
+
+  // Freeing allocated data
+  free(res);
+  free(currentDir);
+  free(changeD);
+
+  return EXIT_SUCCESS;
+}
 
 /**
  *
@@ -116,36 +175,6 @@ int split_line(const char* input_line, char* split_char, char*** res){
   *res = return_tab;
 
   return n_spaces;
-}
-
-
-/**
- * \fn int is_built_in (const char * command)
- *
- * \brief test if the command is built_in
- * \param command string to check
- *
- * \return 0 == TRUE, 1 == FALSE
- */
-int is_built_in(const char* command){
-  /*if(strcmp("cd",command) != 0){
-
-    return 0;
-  } else if(strcmp("pwd", command) != 0){
-
-    return 0;
-  } else if(strcmp("exit", command) != 0){
-
-    return 0;
-  } else if(strcmp("echo", command) != 0){
-
-    return 0;
-  } else {
-    return 1; // TODO
-  }*/
-
-  //TEMP
-  return 1;
 }
 
 /**
@@ -558,75 +587,23 @@ int init_pipe (int in, int out, char * input_line)
   pid_t pid;
 
   if ((pid = fork ()) == 0)
+  {
+    if (in != 0)
     {
-      if (in != 0)
-        {
-          dup2 (in, 0);
-          close (in);
-        }
-
-      if (out != 1)
-        {
-          dup2 (out, 1);
-          close (out);
-        }
-
-
-      main_exec(input_line);
-
+      dup2 (in, 0);
+      close (in);
     }
+
+    if (out != 1)
+    {
+      dup2 (out, 1);
+      close (out);
+    }
+    main_exec(input_line);
+
+  } else if( pid == -1){
+    perror("");
+  }
 
   return pid;
-}
-
-
-/**
- * \fn int fork_pipes (int n, char ** input_line)
- *
- * \brief execute the list of input sequentially and pipe each output in the next input
- * \param n number of iteration
- * \param **input_line array of string to work on
- *
- * \return execution result : 0 = OK
- */
-int fork_pipes (int n, char ** input)
-{
-  int i, res;
-  pid_t pid;
-  int in, fd [2];
-
-  /* The first process should get its input from the original file descriptor 0.  */
-  in = 0;
-
-  /* Note the loop bound, we spawn here all, but the last stage of the pipeline.  */
-  for (i = 0; i < n - 1; ++i)
-    {
-      pipe (fd);
-
-      /* f [1] is the write end of the pipe, we carry `in` from the prev iteration.  */
-      init_pipe (in, fd [1], input[i]);
-
-      /* No need for the write end of the pipe, the child will write here.  */
-      close (fd [1]);
-
-      /* Keep the read end of the pipe, the next child will read from there.  */
-      in = fd [0];
-    }
-
-  /* Last stage of the pipeline - set stdin be the read end of the previous pipe
-     and output to the original file descriptor 1. */  
-  if (in != 0)
-    dup2 (in, 0);
-
-  /* Execute the last stage with the current process. */
-
-  res = main_exec(input [i]);
-
-  close(in);
-  close(fd[1]);
-  close(fd[2]);
-  dup2(0,0);
-  dup2(1,1);
-
-  return res;
 }
